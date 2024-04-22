038696ae05003a9e0e1503f023dfa9f3463956eb
Zolpidem05 <fadoua.err005@gmail.com>
2024-04-22 00:56:36 +0000

    Started to create contoller's functions

--- app/Http/Controllers/AdministrationController.php
+++ app/Http/Controllers/AdministrationController.php
@@ -6,5 +6,322 @@ use Illuminate\Http\Request;
 
 class AdministrationController extends Controller
 {
-    //
+    //CRUD operations on Continents
+    public function create_continent()
+    {
+        
+    }
+    public function read_continent()
+    {
+        
+    }
+    public function update_continent()
+    {
+        
+    }
+    public function delete_continent()
+    {
+        
+    }
+    //CRUD operations on Countries
+    public function create_county()
+    {
+        
+    }
+    public function read_county()
+    {
+        
+    }
+    public function update_county()
+    {
+        
+    }
+    public function delete_county()
+    {
+        
+    }
+    //CRUD operations on Cities
+    public function create_city()
+    {
+        
+    }
+    public function read_city()
+    {
+        
+    }
+    public function update_city()
+    {
+        
+    }
+    public function delete_city()
+    {
+        
+    }
+    //CRUD operations on Categories
+    public function create_category()
+    {
+        
+    }
+    public function read_category()
+    {
+        
+    }
+    public function update_category()
+    {
+        
+    }
+    public function delete_category()
+    {
+        
+    }
+    //CRUD operations on Subcategories
+    public function create_subcategory()
+    {
+        
+    }
+    public function read_subcategory()
+    {
+        
+    }
+    public function update_subcategory()
+    {
+        
+    }
+    public function delete_subcategory()
+    {
+        
+    }
+    //CRUD operations on Statuses
+    public function create_status()
+    {
+        
+    }
+    public function read_status()
+    {
+        
+    }
+    public function update_status()
+    {
+        
+    }
+    public function delete_status()
+    {
+        
+    }
+    //CRUD operations on Messages types
+    public function create_message_type()
+    {
+        
+    }
+    public function read_message_type()
+    {
+        
+    }
+    public function update_message_type()
+    {
+        
+    }
+    public function delete_message_type()
+    {
+        
+    }
+    //CRUD operations on Administarors Roles
+    public function create_admin_role()
+    {
+        
+    }
+    public function read_admin_role()
+    {
+        
+    }
+    public function update_admin_role()
+    {
+        
+    }
+    public function delete_admin_role()
+    {
+        
+    }
+    //CRUD operations on Blog posts
+    public function create_blog_post()
+    {
+        
+    }
+    public function read_blog_post()
+    {
+        
+    }
+    public function update_blog_post()
+    {
+        
+    }
+    public function delete_blog_post()
+    {
+        
+    }
+    //CRUD operations on Profils
+    public function create_artist_profil()
+    {
+        
+    }
+    public function read_artist_profil()
+    {
+        
+    }
+    public function update_artist_profil()
+    {
+        
+    }
+    public function delete_artist_profil()
+    {
+        
+    }
+    //CRUD operations on Artworks
+    public function create_artwork()
+    {
+        
+    }
+    public function read_artwork()
+    {
+        
+    }
+    public function update_artwork()
+    {
+        
+    }
+    public function delete_artwork()
+    {
+        
+    }
+    //CRUD operations on Clients
+    public function create_client()
+    {
+        
+    }
+    public function read_client()
+    {
+        
+    }
+    public function update_client()
+    {
+        
+    }
+    public function delete_client()
+    {
+        
+    }
+    //CRUD operations on Clients comments
+    public function read_client_message()
+    {
+        
+    }
+    public function delete_client_message()
+    {
+        
+    }
+    //CRUD operations on Visitors
+    public function read_visitor()
+    {
+        
+    }
+    public function delete_visitor()
+    {
+        
+    }
+    //CRUD operations on Visitors Messages
+    public function read_visitor_message()
+    {
+        
+    }
+    public function delete_visitor_message()
+    {
+        
+    }
+    //CRUD operations on Administrators
+    public function create_admin()
+    {
+        
+    }
+    public function read_admin()
+    {
+        
+    }
+    public function update_admin()
+    {
+        
+    }
+    public function delete_admin()
+    {
+        
+    }
+    //CRUD operations on Chat Messages
+    public function create_chat_message()
+    {
+        
+    }
+    public function read_chat_message()
+    {
+        
+    }
+    public function update_chat_message()
+    {
+        
+    }
+    public function delete_chat_message()
+    {
+        
+    }
+    //CRUD operations on Analytics Notes
+    public function create_note()
+    {
+        
+    }
+    public function read_note()
+    {
+        
+    }
+    public function update_note()
+    {
+        
+    }
+    public function delete_note()
+    {
+        
+    }
+    //CRUD operations on Transactions
+    public function create_transaction()
+    {
+        
+    }
+    public function read_transaction()
+    {
+        
+    }
+    public function update_transaction()
+    {
+        
+    }
+    public function delete_transaction()
+    {
+        
+    }
+    //CRUD operations on Payments
+    public function create_payment_method()
+    {
+        
+    }
+    public function read_payment_method()
+    {
+        
+    }
+    public function update_payment_method()
+    {
+        
+    }
+    public function delete_payment_method()
+    {
+        
+    }
+
+
 }
