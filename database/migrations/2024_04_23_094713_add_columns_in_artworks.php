a14b18d6bf440e423874d6afaa3f9f5619f421b0
Zolpidem05 <fadoua.err005@gmail.com>
2024-04-23 09:50:45 +0000

    Rectified columns and tables in the migrations and models 3

---
+++ database/migrations/2024_04_23_094713_add_columns_in_artworks.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('artwork_status');
+            $table->foreign('artwork_status')
+                ->references('status_id')
+                ->on('statuses')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('artworks', function (Blueprint $table) {
+            //
+        });
+    }
+};
